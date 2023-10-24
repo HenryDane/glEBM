18f254fcb04505104df506270468fb07bd35cbf8
HenryDane <aglareb7@gmail.com>
2023-10-24 13:00:43 -0700

    re-introduced adv-diff

--- shader/compute.cs
+++ shader/compute.cs
@@ -28,12 +28,15 @@ const float olr_B =   2.0f;
 const float ice_FB     =   0;
 const float ice_B      =   2.83;
 const float ice_ki     =   1.981;
-const float ice_c0H0   =   2.0e8;
+const float h2o_c0     =   4.0e6;
+const float ice_c0     =   1.919e6;
+const float depth      =  50.0;
 const float ice_Li     =   3.0e8;
 const float ice_Tm     = 273.15;
 const float ice_alpha  =   0.56;
 const float ice_deltaa =   0.48;
 const float ice_ha     =   0.5;
+const float ice_c0H0   = h2o_c0 * depth;
 
 float deg2rad(float x) {
     return pi / 180.0f * x;
@@ -74,7 +77,7 @@ float calc_OLR(float T, float A) {
     return A + (olr_B * (T - 273.15));
 }
 
-vec4 calc_merid_advdiff(vec4 N, ivec2 coord, float lat, float C) {
+vec4 calc_merid_advdiff(vec4 N, ivec2 coord, float lat, float K_j, float K_jp1) {
     const float D = 0.555;
     ivec2 imgsize = imageSize(stateOut);
 
@@ -97,8 +100,9 @@ vec4 calc_merid_advdiff(vec4 N, ivec2 coord, float lat, float C) {
 
     float W_i    = cos(phi);
 
-    float K_j    = D / C * Re * Re;
-    float K_jp1  = D / C * Re * Re;
+    // coordinate system conversion
+    K_j         *= Re * Re;
+    K_jp1       *= Re * Re;
 
     float U_j    = 0;
     float U_jp1  = 0;
@@ -114,6 +118,31 @@ vec4 calc_merid_advdiff(vec4 N, ivec2 coord, float lat, float C) {
     float Tu = (Wb_jp1 / W_i) * (K_jp1 - U_jp1 * (Xb_jp1 - X_i)) / ((Xb_jp1 - Xb_j) * (X_ip1 - X_i));
 
     return (Tl * N_im1 + Tm * N + Tu * N_ip1) + S_i;
+}
+
+float calc_K(vec4 value) {
+    // thermal conductivity
+    const float D_ice = 2.22;
+    const float D_h2o = 0.555;
+
+    // calculate h_ice
+    float h_ice = -value.g / ice_Li * float(value.g < 0);
+
+    // calc effective specific heat
+    float C = 0;
+    C += depth * ice_c0 * float(h_ice >  0);
+    C += depth * h2o_c0 * float(h_ice <= 0);
+
+    // calculate effective thermal conductivity
+    float D = 0;
+    D += D_ice * float(h_ice >  0);
+    D += D_h2o * float(h_ice <= 0);
+
+    // calculate K values
+    // only return K != 0 iif the cell contains liquid water
+    // since I don't think it makes sense to diffuse the ice itself
+    // but I'm not sure...
+    return D / C * float(value.g > 0);
 }
 
 void main() {
@@ -133,6 +162,18 @@ void main() {
 
     // compute albedo
     float alpha = calc_albedo(value.r, lat);
+
+    // compute K values
+    float K_p   = calc_K(imageLoad(stateOut, texelCoord + ivec2(0,  1)));
+    float K_m   = calc_K(imageLoad(stateOut, texelCoord + ivec2(0, -1)));
+    float K_i   = calc_K(value);
+    float K_j   = 0.5 * (K_i + K_m);
+    float K_jp1 = 0.5 * (K_i + K_p);
+
+    // advect E
+    float dEdt_diff = calc_merid_advdiff(value, texelCoord, lat, K_j, K_jp1).g;
+    value.g += dEdt_diff * dt * secs_per_day;
+    value.a = K_j;
 
     // compute change in E
     float ASR  = calc_ASR(alpha, Q);
